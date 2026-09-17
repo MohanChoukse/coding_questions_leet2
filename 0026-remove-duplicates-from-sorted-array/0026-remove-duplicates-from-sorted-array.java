a1b3370eafd7daee64c213f044e0c35f01641969
Mohan Chouksey <162291835+MohanChoukse@users.noreply.github.com>
2026-09-17 22:49:19 +0530

    Time: 1 ms (77.03%), Space: 46.5 MB (81.16%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -1,14 +1,11 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-
-        int j = 1;
         int i = 0;
+        int j = 1;
         if (nums.length == 0) {
             return 0;
         }
-
         while (j < nums.length) {
-
             if (nums[i] == nums[j]) {
                 j++;
             } else {
@@ -16,7 +13,6 @@ class Solution {
                 nums[i] = nums[j];
                 j++;
             }
-
         }
         return i + 1;
     }
